e108a6aa2f68f86bcbe3d3dc73bc18e452608170
fabrizzio-gz <fabrizzio@chartmetric.com>
2023-08-27 11:56:37 -0400

    feat: add author seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Author.destroy_all
+
+authors = Author.create!([
+  {
+    name: "Gautama Buddha",
+    yob: 483,
+    is_bce: true
+  },
+  {
+    name: "Ray Bradbury",
+    yob: 1920
+  },
+  {
+    name: "Pema Chödrön",
+    yob: 1936
+  },
+  {
+    name: "Thich Nhat Hanh",
+    yob: 1926
+  }
+])
+
+puts "Created #{authors.length} authors"
