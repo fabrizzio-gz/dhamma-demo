9e0f7b0e0e4c30d564c32c82267c646372bca53e
fabrizzio-gz <fabrizzio@chartmetric.com>
2023-08-27 12:11:15 -0400

    feat(model:author): add year_of_birth method

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
 class Author < ApplicationRecord
+
+  def year_of_birth
+    if yob.nil?
+      return nil
+    end
+
+    bce_suffix = is_bce ? " (BCE)" : ""
+    return "#{yob}#{bce_suffix}"
+  end
+
 end
